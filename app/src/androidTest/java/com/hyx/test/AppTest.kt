3e5525214c111ef70b0d95f2e108f7d6d024dce7
huyuxiang <1293158932@qq.com>
2020-08-21 11:09:42 +0800

    添加单元测试：测试数据库查询

---
+++ app/src/androidTest/java/com/hyx/test/AppTest.kt
@@ -0,0 +1,29 @@
+package com.hyx.test
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.hyx.test.db.DbManage.Companion.getInstance
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppTest {
+    @Test
+    fun testGetDatasByPage() {
+        val ctx: Context? = InstrumentationRegistry.getInstrumentation().context
+        val list = getInstance(ctx!!)!!.getDatasByPage(0)
+        if (list != null) {
+            for (entity in list) {
+                println(entity)
+            }
+        }
+    }
+
+    @Test
+    fun testGetLastData() {
+        val ctx: Context? = InstrumentationRegistry.getInstrumentation().context
+        val entity = getInstance(ctx!!)!!.lastData
+        println(entity)
+    }
+}
