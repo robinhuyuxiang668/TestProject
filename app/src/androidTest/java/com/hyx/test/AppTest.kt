222d19849d8b2c94b5fe92a174a6a6a6cc5a0343
huyuxiang <1293158932@qq.com>
2020-08-26 19:02:11 +0800

    添加UI测试：主界面跳转历史界面 ；历史记录下拉获取最新数据和上拉获取下一页数据

--- app/src/androidTest/java/com/hyx/test/AppTest.kt
+++ app/src/androidTest/java/com/hyx/test/AppTest.kt
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class AppTest {
     @Test
-    fun testGetDatasByPage() {
+    fun testGetDatasByPage() {1
         val ctx: Context? = InstrumentationRegistry.getInstrumentation().context
         val list = getInstance(ctx!!)!!.getDatasByPage(0)
         if (list != null) {
